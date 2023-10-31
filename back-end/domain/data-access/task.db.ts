c2e98364180804fc6a634a938f08c220f26f2017
Dieter Daems <dieter.daems1985@gmail.com>
2023-10-31 14:34:46 +0100

    added update and create to db

--- back-end/domain/data-access/task.db.ts
+++ back-end/domain/data-access/task.db.ts
@@ -1,3 +1,4 @@
+import { TaskInput } from "../../types";
 import prisma from "../../util/init-db";
 import { Task } from "../model/task";
 
@@ -23,4 +24,28 @@ const getTaskById = async (id: number): Promise<Task> => {
     }
 }
 
-export default { getAllTasks, getTaskById };
+const createTask = async ({name, id, description, deadline}: TaskInput): Promise<Task> => {
+    try {
+        const newTask = new Task({name, id, description, deadline});
+        const taskPrisma = await prisma.task.create({ data: { name, id, description, deadline } });
+        const task = Task.from(taskPrisma);
+        return task;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+const updateTask = async ({name, id, description, deadline}: TaskInput): Promise<Task> => {
+    try {
+        const newTask = new Task({name, id, description, deadline});
+        const taskPrisma = await prisma.task.update({ where: { id: id }, data: { name, id, description, deadline } });
+        const task = Task.from(taskPrisma);
+        return task;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+export default { getAllTasks, getTaskById, createTask, updateTask };
