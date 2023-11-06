382795aebee364b9a8507c42e24c9c0945e7005d
Ibra Altaee <altaee.ibra@gmail.com>
2023-11-07 00:30:10 +0100

    user db adjusted to use prisma & different gets + createUser added

--- back-end/domain/data-access/user.db.ts
+++ back-end/domain/data-access/user.db.ts
@@ -1,18 +1,65 @@
 import { User } from "../model/user";
+import prisma from "../../util/init-db";
 
-const users: User[] = [
-    new User({ name: 'Stijnje', specialisation: 'Ai', email: 'stijn@test.be', password: 't' }),
-    new User({ name: 'Sander', specialisation: 'Backend', email: 'sander@test.be', password: 't' })
-    ,
-];
-
-const getAllUsers = (): User[] => users;
-const getUserByEmail = (email: string): User => users.find((u) => u.email === email);
-
-const createUser = ({name, specialisation, email, password} : User): User => {
-    const user = new User({name, specialisation, email, password});
-    users.push(user);
-    return user;
+const getAllUsers = async (): Promise<User[]> => {
+    try {
+        const usersPrisma = await prisma.user.findMany();
+        if (!usersPrisma) throw new Error(`No users found.`);
+        const users = usersPrisma.map((userPrisma) => User.from(userPrisma));
+        return users;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
 }
 
-export default { getAllUsers, getUserByEmail, createUser };
+const getUserByEmail = async (email: string): Promise<User> => {
+    try {
+        const userPrisma = await prisma.user.findUnique({
+            where:
+            { email: email }
+        });
+        if (!userPrisma) throw new Error(`User with email ${email} does not exist.`);
+        const user = User.from(userPrisma);
+        return user;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+const getUserById = async (id: number): Promise<User> => {
+    try {
+        const userPrisma = await prisma.user.findUnique({
+            where:
+            { id: id }
+        });
+        if (!userPrisma) throw new Error(`User with id ${id} does not exist.`);
+        const user = User.from(userPrisma);
+        return user;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+const createUser = async ({name, specialisation, email, password}: User): Promise<User> => {
+    try {
+        const userPrisma = await prisma.user.create({
+            data: {
+                name,
+                specialisation,
+                email,
+                password,
+            }
+        });
+        const user = User.from(userPrisma);
+        return user;
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
+
+
+export default { getAllUsers, getUserByEmail, getUserById, createUser };
