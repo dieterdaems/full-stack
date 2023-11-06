7655a6a2def434df34f5834789c3080ee6ec6619
Ibra Altaee <altaee.ibra@gmail.com>
2023-11-06 23:46:44 +0100

    team model & service adjusted, createTeam & addUserToTeam added

--- back-end/domain/model/team.ts
+++ back-end/domain/model/team.ts
@@ -2,17 +2,25 @@ import { User } from "./user";
 
 export class Team {
     readonly name: string;
-    readonly id?: number;
     readonly users: Array<User>;
+    readonly id?: number;
 
-    constructor(team: {name: string, id?:number, users:Array<User>}) {
+    constructor(team: {name: string, users: User[], id? :number}) {
         this.name = team.name;
-        this.id = team.id;
         this.users = team.users;
+        this.id = team.id;
     }
 
-    equals(id:number): boolean{
-        return this.id === id;
+    equals(team: Team): boolean{
+        return this.name === team.name;
+    }
+
+    addUserToTeam(user: User): void{
+        if (this.users.includes(user))
+        {
+            throw new Error("User already in team");
+        }
+        this.users.push(user);
     }
 }
 
