1bd5823678927c9765744dc80ed92991f0807fed
Ibra Altaee <altaee.ibra@gmail.com>
2023-12-26 16:07:48 +0100

    Frontend team overview page moved under users

--- front-end/pages/users/teams/[id].tsx
+++ front-end/pages/users/teams/[id].tsx
@@ -1,11 +1,14 @@
 import TeamsOverviewTabel from "@/components/teams/TeamsOverviewTabel";
 import TeamService from "@/services/TeamService";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import useSWR, { mutate } from "swr";
 import useInterval from "use-interval";
 
 const Teams: React.FC = () => {
 
+    const router = useRouter();
+    const { id } = router.query;
 
     const getAllTeams = async () => {
         const response = await TeamService.getAll();
@@ -18,6 +21,7 @@ const Teams: React.FC = () => {
         mutate('allTeams', getAllTeams());
     }, 1000);
 
+    
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
     return (
@@ -26,8 +30,8 @@ const Teams: React.FC = () => {
                 <title>Teams</title>
             </Head>
             <main>
-                        <h1>Teams</h1>
-                        {<TeamsOverviewTabel teams={data} />}
+                <h1>Teams</h1>
+                {<TeamsOverviewTabel teams={data} id={parseInt(id as string)}/>}
             </main>
         </>
     )
