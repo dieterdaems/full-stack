48e0c4b3f0bac495a8a3ff8c75bba854c25570f9
Ibra Altaee <altaee.ibra@gmail.com>
2023-12-26 16:08:39 +0100

    Frontend Team overview table adjusted to add users

--- front-end/services/TeamService.ts
+++ front-end/services/TeamService.ts
@@ -7,8 +7,18 @@ const getAll = () => {
     });
 }
 
+const addUser = (teamId: number, userId: number) => {
+    return fetch(process.env.NEXT_PUBLIC_API_URL + '/teams/' + teamId + '/user/' + userId, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
 const TeamService = {
     getAll,
+    addUser
 }
 
 export default TeamService;
