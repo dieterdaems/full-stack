6abce18e534bebb1320979700ad84cd07c75cda8
Dieter Daems <dieter.daems1985@gmail.com>
2023-12-28 12:45:56 +0100

    added loc for project page

--- front-end/components/projects/AddProject.tsx
+++ front-end/components/projects/AddProject.tsx
@@ -1,11 +1,13 @@
 import ProjectService from '@/services/ProjectService';
 import { Project } from '@/types';
 import { time } from 'console';
+import { useTranslation } from 'next-i18next';
 import React, { FormEvent, useState } from 'react';
 import { setTimeout } from 'timers/promises';
 
 const AddProject: React.FC = () => {
 
+    const { t } = useTranslation();
     const [name, setName] = useState<string>("");
     const [button, setButton] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState('');
@@ -33,7 +35,8 @@ const AddProject: React.FC = () => {
                 setName('');
             } else {
                 // console.log('Project not created');
-                setErrorMessage('Project not created'); 
+                setErrorMessage('Project not created');
+                
             }
 
         }
@@ -41,11 +44,11 @@ const AddProject: React.FC = () => {
 
     return (
         <>  
-        <button onClick={(e) => setButton(!button)}>Add Project</button>
+        <button onClick={(e) => setButton(!button)}>{t('projects.new')}</button>
         {button && <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">{t('projects.name')}</label>
             <input type="text" id="name" onChange={(e) => setName(e.target.value)} />
-            <button type='submit'>Submit</button>
+            <button type='submit'>{t('projects.submit')}</button>
         </form>}
         {errorMessage && <p>{errorMessage}</p>}
         </>
--- front-end/pages/projects/index.tsx
+++ front-end/pages/projects/index.tsx
@@ -2,15 +2,18 @@ import AddProject from "@/components/projects/AddProject";
 import ProjectOverviewTable from "@/components/projects/ProjectsOverviewTable";
 import ProjectService from "@/services/ProjectService";
 import { Project } from "@/types";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Head } from "next/document";
 import { mainModule } from "process";
 import { useEffect, useState, useDeferredValue } from "react";
+import { useTranslation } from "next-i18next";
 import useSWR, { mutate } from "swr";
 import useInterval from "use-interval";
 
 const Projects: React.FC = () => {
 
-
+    const {t} = useTranslation();
     const fetchProjects = async () => {
         
         const response = await ProjectService.getAll();
@@ -44,9 +47,9 @@ useInterval(() => {
 return (
     <>
     <main>
-        <h1>Projects</h1>
+        <h1>{t('projects.title')}</h1>
         {error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <p>{t('projects.loading')}</p>}
         <section>
             {data && (<ProjectOverviewTable projects={data} />) }
         </section>
@@ -61,5 +64,11 @@ return (
 )
 
     };
+
+    export const getServerSideProps: GetServerSideProps = async ({locale}) =>({
+        props: {
+          ...(await serverSideTranslations(locale ?? "en" ,["common"])),
+        },
+      })
 
 export default Projects;
