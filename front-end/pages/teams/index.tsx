51a85fcf7d5748ba6427d67bba06f63f0e0e37b9
Ibra Altaee <altaee.ibra@gmail.com>
2023-12-29 20:51:05 +0100

    Frontend teams&users overview authorisation

--- front-end/pages/teams/index.tsx
+++ front-end/pages/teams/index.tsx
@@ -2,44 +2,71 @@ import TeamsOverviewTable from "@/components/teams/TeamsOverviewTable";
 import TeamService from "@/services/TeamService";
 import UserService from "@/services/UserService";
 import Head from "next/head";
+import { useState } from "react";
 import useSWR, { mutate } from "swr";
 import useInterval from "use-interval";
 
 const Teams: React.FC = () => {
 
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
     const getAllTeams = async () => {
         const response = await TeamService.getAll();
-        return response.json();
+        const data = await response.json();
+        if (!response.ok) {
+            if (response.status === 401) {
+                setErrorMessage("You are not authorized to view this page.");
+            }
+            else setErrorMessage(data.errorMessage);
+        }
+        else {
+            return data;
+        }
     }
-
     const getUserTeams = async () => {
+        setErrorMessage("");
         const id = sessionStorage.getItem("loggedUser");
         if (!id) return;
-
         const response = await UserService.getById(parseInt(id));
         const user = await response.json();
-        return user.teams;
-    }
+        if (!response.ok) {
+            if (response.status === 401) {
+                setErrorMessage("You are not authorized to view this page.");
+            }
+            else setErrorMessage(user.errorMessage);
+        }
+        else {
+            return user.teams;
+        }
+    };
+
 
     const { data: currentTeamsData, error: currentTeamsError } = useSWR('currentTeams', getUserTeams);
     const { data, error } = useSWR('allTeams', getAllTeams);
 
     useInterval(() => {
-        mutate('allTeams', getAllTeams());
-        mutate('currentTeams', getUserTeams());
+        if (!errorMessage) {
+            mutate('allTeams', getAllTeams());
+            mutate('currentTeams', getUserTeams());
+        }
     }, 1000);
 
-    
+
     if (error || currentTeamsError) return <>Failed to load</>
-    if (!data || !currentTeamsData) return <>Loading...</>
     return (
         <>
             <Head>
                 <title>Teams</title>
             </Head>
             <main>
-                <h1>Teams</h1>
-                {<TeamsOverviewTable teams={data} currentTeams={currentTeamsData}/>}
+                <p>{errorMessage}</p>
+                {data && currentTeamsData && (
+                    <>
+                        <h1>Teams</h1>
+                        {<TeamsOverviewTable teams={data} currentTeams={currentTeamsData} />}
+                    </>
+                )}
+                {!data && !errorMessage && <p>Loading...</p>}
             </main>
         </>
     )
--- front-end/pages/users/index.tsx
+++ front-end/pages/users/index.tsx
@@ -1,35 +1,53 @@
 import UsersOverviewTable from "@/components/users/UsersOverviewTable";
 import UserService from "@/services/UserService";
 import Head from "next/head";
+import { useState } from "react";
 import useSWR, { mutate } from "swr";
 import useInterval from "use-interval";
 
 
 const Users: React.FC = () => {
 
+    const [statusMessage, setStatusMessage] = useState<string>("");
+
     const getAllUsers = async () => {
+        setStatusMessage("");
         const response = await UserService.getAll();
-        return response.json();
+        const users = await response.json();
+        if (!response.ok) {
+            if (response.status === 401) {
+                setStatusMessage("You are not authorized to view this page.");
+            }
+            else setStatusMessage(users.errorMessage);
+        }
+        else {
+            return users;
+        }
     }
 
     const { data, error } = useSWR('allUsers', getAllUsers);
 
     useInterval(() => {
-        mutate('allUsers', getAllUsers());
+        if (data) mutate('allUsers', getAllUsers());
     }, 1000);
 
     if (error) return <>Failed to load</>
-    if (!data) return <>Loading...</>
     return (
         <>
             <Head>
                 <title>Users</title>
             </Head>
             <main>
-                <h1>Users</h1>
-                <section>
-                    {<UsersOverviewTable users={data}/>}
-                </section>
+                <p>{statusMessage}</p>
+                {data && (
+                    <>
+                        <h1>Users</h1>
+                        <section>
+                            {<UsersOverviewTable users={data} />}
+                        </section>
+                    </>
+                )}
+                {!data && !statusMessage && <p>Loading...</p>}
             </main>
         </>
     );
