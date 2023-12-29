747f505ff87c4ebce416911ffac7bad8a26bba09
Ibra Altaee <altaee.ibra@gmail.com>
2023-12-29 02:22:56 +0100

    Frontend teams overview updated to have del button

--- front-end/services/TeamService.ts
+++ front-end/services/TeamService.ts
@@ -23,10 +23,23 @@ const create = (name: string) => {
     });
 }
 
+const deleteById = (id: number) => {
+    const token = sessionStorage.getItem('token');
+    return fetch(process.env.NEXT_PUBLIC_API_URL + '/teams/delete/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+
+        },
+    });
+};
+
 
 const TeamService = {
     getAll,
-    create
+    create,
+    deleteById
 }
 
 export default TeamService;
