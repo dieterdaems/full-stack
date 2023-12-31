ef0b7fefba2de8313445bd02a9842e3b893544a3
Ibra Altaee <altaee.ibra@gmail.com>
2023-12-31 02:36:25 +0100

    Frontend only show login&regis pages if not logged

--- front-end/pages/login/index.tsx
+++ front-end/pages/login/index.tsx
@@ -1,23 +1,47 @@
 import UserLoginForm from "@/components/users/UserLoginForm";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 
 const UserLogin: React.FC = () => {
+
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    // Redirect to homepage if the user is already logged in
+    useEffect(() => {
+        // Check if the JWT token is present in sessionStorage
+        const token = sessionStorage.getItem('token');
+
+        // If the token is present, redirect the user to another page
+        if (token) {
+            router.push('/');
+        }
+        else {
+            // If the token is not present, set loading to false to render the content
+            setLoading(false);
+        }
+    }, [router]);
+
+
     return (
         <>
             <Head>
                 <title>Login</title>
             </Head>
             <main>
-                <h1>Login</h1>
-                <section>
-                    {<UserLoginForm />}
-                </section>
+                {!loading && (
+                    <>
+                        <h1>Login</h1>
+                        <section>
+                            {<UserLoginForm />}
+                        </section>
+                    </>
+                )}
             </main>
-
         </>
     )
-
 }
 
 export default UserLogin;
--- front-end/pages/register/index.tsx
+++ front-end/pages/register/index.tsx
@@ -1,18 +1,43 @@
 import UserRegistrationForm from "@/components/users/UserRegistrationForm";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 
 const UserRegistration: React.FC = () => {
+
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    // Redirect to homepage if the user is already logged in
+    useEffect(() => {
+        // Check if the JWT token is present in sessionStorage
+        const token = sessionStorage.getItem('token');
+
+        // If the token is present, redirect the user to another page
+        if (token) {
+            router.push('/');
+        }
+        else {
+            // If the token is not present, set loading to false to render the content
+            setLoading(false);
+        }
+    }, [router]);
+
     return (
         <>
             <Head>
                 <title>Registration</title>
             </Head>
             <main>
-                <h1>User Registration</h1>
-                <section>
-                    {<UserRegistrationForm />}
-                </section>
+                {!loading && (
+                    <>
+                        <h1>User Registration</h1>
+                        <section>
+                            {<UserRegistrationForm />}
+                        </section>
+                    </>
+                )}
             </main>
 
         </>
