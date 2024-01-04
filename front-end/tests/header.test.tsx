5d4d1b148ee5db5cc362ae884c3b0226e23b6fd7
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-01-04 13:56:25 +0100

    tests for header component

--- front-end/tests/header.test.tsx
+++ front-end/tests/header.test.tsx
@@ -1,55 +1,57 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import Header from '@/components/header';
+import { render, screen } from '@testing-library/react';
 import { useRouter } from 'next/router';
-import Header from "../components/header";
-import React from "react";
+import React from 'react';
+
 
 window.React = React;
 
-
-const sessionStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn(),
-  removeItem: jest.fn(),
-  key: jest.fn(),
-  length: 0,
-};
-global.sessionStorage = sessionStorageMock;
-
 jest.mock('next/router', () => ({
-    __esModule: true,
-    useRouter: jest.fn(),
-  }));
+  useRouter: jest.fn(),
+}));
 
-    test("renders the navigation links correctly", () => {
-        (useRouter as jest.Mock).mockImplementation(() => ({
-            pathname: '/',
-            push: jest.fn(),
-          }));
-        //when
-        render(<Header />);
-
-        //then
-        expect(screen.getByText("Home"));
-        expect(screen.getByText("Login"));
-        expect(screen.getByText("Register"));
+describe('Header', () => {
+  it('shows the correct links when logged in as admin', () => {
+    (useRouter as jest.Mock).mockReturnValue({
+      pathname: '/',
+      query: {},
+      asPath: '/',
     });
+    sessionStorage.setItem('loggedUser', 'admin');
+    sessionStorage.setItem('role', '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8');
+    render(<Header />);
+    expect(screen.getByText('Users'))
+    expect(screen.getByText('Teams'))
+    expect(screen.getByText('Projects'))
+    expect(screen.getByText('Profile'))
+    expect(screen.getByText('Logout'))
+  });
 
+  it('shows the correct links when logged in as non-admin', () => {
+    (useRouter as jest.Mock).mockReturnValue({
+      pathname: '/',
+      query: {},
+      asPath: '/',
+    });
+    sessionStorage.setItem('loggedUser', 'user');
+    sessionStorage.setItem('role', '4b975fd8f0ff3e9fe958e701d5053be7dc223b684ec633f3d322d8868d395d33');
+    render(<Header />);
+    expect(screen.getByText('Teams'))
+    expect(screen.getByText('Projects'))
+    expect(screen.getByText('Profile'))
+    expect(screen.getByText('Logout'))
+  });
 
-    // Doesnt show the links when logged in in test
-    // test("renders the navigation links correctly when logged in", () => {
-    //     (useRouter as jest.Mock).mockImplementation(() => ({
-    //         pathname: '/',
-    //         push: jest.fn(),
-    //       }));
-    //       sessionStorageMock.setItem('LoggedIn', 1);
-
-    //     //when
-    //     render(<Header />);
-
-    //     //then
-    //     expect(screen.getByText("Home"));
-    //     expect(screen.getByText("Projects"));
-    //     expect(screen.getByText("Profile"));
-    // });
-
+  it('shows the correct links when not logged in', () => {
+    (useRouter as jest.Mock).mockReturnValue({
+      pathname: '/',
+      query: {},
+      asPath: '/',
+    });
+    sessionStorage.clear();
+    render(<Header />);
+    expect(screen.getByText('Home'))
+    expect(screen.queryByText('Login'))
+    expect(screen.queryByText('Register'))
+  });
+});
