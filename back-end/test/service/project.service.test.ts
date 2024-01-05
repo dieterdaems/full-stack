2ee63502b8faf063d42f4b37ed0954c9991fce51
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-01-05 20:29:07 +0100

    fixed project service tests

--- back-end/test/service/project.service.test.ts
+++ back-end/test/service/project.service.test.ts
@@ -41,21 +41,21 @@ afterAll(() => {
 //tests for getAllProjects
 
 test('given valid projects, when getAllProjects is called as admin, then all projects are returned', async () => {
-    projectDb.getAllProjects = mockProjectsDbGetAllProjects.mockReturnValue([project1, project2]);
+    projectDb.getAllProjects = mockProjectsDbGetAllProjects.mockResolvedValue([project1, project2]);
     const projects = await projectService.getAllProjects({id: 1, role: 'admin', currentUser: 1});
     expect(projects).toEqual([project1, project2]);
 }
 )
 
 test('given valid projects, when getAllProjects is called as user, then all projects of user are returned', async () => {
-    projectDb.getAllProjectsByUserId = mockProjectsDbGetAllProjectsByUserId.mockReturnValue([project1]);
+    projectDb.getAllProjectsByUserId = mockProjectsDbGetAllProjectsByUserId.mockResolvedValue([project1]);
     const projects = await projectService.getAllProjects({id: 1, role: 'user', currentUser: 1});
     expect(projects).toEqual([project1]);
 }
 )
 
 test('given valid projects, when getAllProjects is called as user with different id, then error is thrown', async () => {
-    projectDb.getAllProjectsByUserId = mockProjectsDbGetAllProjectsByUserId.mockReturnValue([project1]);
+    projectDb.getAllProjectsByUserId = mockProjectsDbGetAllProjectsByUserId.mockResolvedValue([project1]);
     await expect(projectService.getAllProjects({id: 2, role: 'user', currentUser: 1})).rejects.toThrowError('You are not authorized to access this resource.');
 }
 )
@@ -63,15 +63,15 @@ test('given valid projects, when getAllProjects is called as user with different
 // //tests for getProjectById
 
 // test('given valid project, when getProjectById is called as admin, then project is returned', async () => {
-//     projectDb.getProjectById = jest.fn().mockReturnValue(project1);
+//     projectDb.getProjectById = jest.fn().mockResolvedValue(project1);
 //     const project = await projectService.getProjectById({id: 1, role: 'admin', currentUser: 1});
 //     expect(project).toEqual(project1);
 // }
 // )
 
 // test('given valid project, when getProjectById is called as user, then project is returned', async () => {
-//     projectDb.getProjectById = jest.fn().mockReturnValue(project1);
-//     userDb.getUserById = jest.fn().mockReturnValue(user);
+//     projectDb.getProjectById = jest.fn().mockResolvedValue(project1);
+//     userDb.getUserById = jest.fn().mockResolvedValue(user);
 //     const project = await projectService.getProjectById({id: 1, role: 'user', currentUser: 1});
 //     expect(project).toEqual(project1);
 // }
@@ -80,23 +80,23 @@ test('given valid projects, when getAllProjects is called as user with different
 //tests for createProject
 
 test('given valid project, when createProject is called, then project is returned', async () => {
-    projectDb.getAllProjects = jest.fn().mockReturnValue([project2]);
-    projectDb.createProject = jest.fn().mockReturnValue(project1);
+    projectDb.getAllProjects = mockProjectsDbGetAllProjects.mockResolvedValue([project2]);
+    projectDb.createProject = mockProjectsDbcreateProject.mockResolvedValue(project1);
     const project = await projectService.createProject({name: "project1", team: team});
     expect(project).toEqual(project1);
 }
 )
 
 test('given invalid project, when createProject is called, then error is thrown', async () => {
-    projectDb.getAllProjects = jest.fn().mockReturnValue([project2]);
-    projectDb.createProject = jest.fn().mockReturnValue(project1);
+    projectDb.getAllProjects = mockProjectsDbGetAllProjects.mockResolvedValue([project2]);
+    projectDb.createProject = mockProjectsDbcreateProject.mockResolvedValue(project1);
     await expect(projectService.createProject({name: "", team: team})).rejects.toThrowError('Project name is required.');
 }
 )
 
 test('given valid project with name already in database, when createProject is called, then error is thrown', async () => {
-    projectDb.getAllProjects = jest.fn().mockReturnValue([project1]);
-    projectDb.createProject = jest.fn().mockReturnValue(project1);
+    projectDb.getAllProjects = mockProjectsDbGetAllProjects.mockResolvedValue([project1]);
+    projectDb.createProject = mockProjectsDbcreateProject.mockResolvedValue(project1);
     await expect(projectService.createProject({name: "project1", team: team})).rejects.toThrowError('Project with name project1 already exists.');
 }
 )
@@ -104,29 +104,29 @@ test('given valid project with name already in database, when createProject is c
 //tests for deleteProject
 
 test('given valid project, when deleteProject is called as admin, then project is returned', async () => {
-    projectDb.getProjectById = jest.fn().mockReturnValue(project1);
-    projectDb.deleteById = jest.fn().mockReturnValue(project1);
-    taskDb.getAllTasks = jest.fn().mockReturnValue([]);
-    taskDb.deleteById = jest.fn().mockReturnValue(null);
+    projectDb.getProjectById = mockProjectsDbGetProjectById.mockResolvedValue(project1);
+    projectDb.deleteById = mockTasksDbDeleteById.mockResolvedValue(project1);
+    taskDb.getAllTasks = mockTasksDbGetAllTasks.mockResolvedValue([]);
+    taskDb.deleteById = mockTasksDbDeleteById.mockResolvedValue(null);
     const project = await projectService.deleteProject({id: 1, role: 'admin'});
     expect(project).toEqual(project1);
 }
 )
 
 test('given valid project, when deleteProject is called as user, then error is thrown', async () => {
-    projectDb.getProjectById = jest.fn().mockReturnValue(project1);
-    projectDb.deleteById = jest.fn().mockReturnValue(project1);
-    taskDb.getAllTasks = jest.fn().mockReturnValue([]);
-    taskDb.deleteById = jest.fn().mockReturnValue(null);
+    projectDb.getProjectById = mockProjectsDbGetProjectById.mockResolvedValue(project1);
+    projectDb.deleteById = mockProjectsDbDeleteById.mockResolvedValue(project1);
+    taskDb.getAllTasks = mockTasksDbGetAllTasks.mockResolvedValue([]);
+    taskDb.deleteById = mockTasksDbDeleteById.mockResolvedValue(null);
     await expect(projectService.deleteProject({id: 1, role: 'user'})).rejects.toThrowError('You are not authorized to access this resource.');
 }
 )
 
 test('given invalid project, when deleteProject is called as admin, then error is thrown', async () => {
-    projectDb.getProjectById = jest.fn().mockReturnValue(undefined);
-    projectDb.deleteById = jest.fn().mockReturnValue(project1);
-    taskDb.getAllTasks = jest.fn().mockReturnValue([]);
-    taskDb.deleteById = jest.fn().mockReturnValue(null);
+    projectDb.getProjectById = mockProjectsDbGetProjectById.mockResolvedValue(undefined);
+    projectDb.deleteById = mockProjectsDbDeleteById.mockResolvedValue(project1);
+    taskDb.getAllTasks = mockTasksDbGetAllTasks.mockResolvedValue([]);
+    taskDb.deleteById = mockTasksDbDeleteById.mockResolvedValue(null);
     await expect(projectService.deleteProject({id: 1, role: 'admin'})).rejects.toThrowError('Project with id 1 does not exist.');
 }
 )
