0c1210ce84f472286d06022110c234752ef66147
Ibra Altaee <altaee.ibra@gmail.com>
2024-01-06 04:21:48 +0100

    header localized

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import Language from "./language/Language";
 import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 
 const Header: React.FC = () => {
     const [loggedIn, setLoggedIn] = useState<string | null>(null);
     const [role, setRole] = useState<string | null>(null);
 
     const router = useRouter();
- 
+    const { t } = useTranslation();
+
     const handleLogout = () => {
         sessionStorage.removeItem("loggedUser");
         sessionStorage.removeItem("token");
@@ -30,21 +32,21 @@ const Header: React.FC = () => {
             <nav>
                 <ul  className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
                     <li>
-                        <a className="md:p-4 py-3 px-0 block" href="/">Home</a>
+                        <a className="md:p-4 py-3 px-0 block" href="/">{t('header.nav.home')}</a>
                     </li>
                     {loggedIn && (role === "91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8") && (
                     <li>
-                        <a className="md:p-4 py-3 px-0 block" href="/users">Users</a>
+                        <a className="md:p-4 py-3 px-0 block" href="/users">{t('header.nav.users')}</a>
                     </li>
                     )}
                     {loggedIn && (
                     <li>
-                        <a className="md:p-4 py-3 px-0 block" href="/teams">Teams</a>
+                        <a className="md:p-4 py-3 px-0 block" href="/teams">{t('header.nav.teams')}</a>
                     </li>
                     )}
                     {loggedIn && (
                     <li>
-                        <a className="md:p-4 py-3 px-0 block" href="/projects">Projects</a>
+                        <a className="md:p-4 py-3 px-0 block" href="/projects">{t('header.nav.projects')}</a>
                     </li>
                     )}
                     {/* {loggedIn && (
@@ -54,20 +56,20 @@ const Header: React.FC = () => {
                     )} */}
                     {loggedIn && (
                         <li>
-                            <a className="md:p-4 py-3 px-0 block" href={"/users/profile/"}>Profile</a>
+                            <a className="md:p-4 py-3 px-0 block" href={"/users/profile/"}>{t('header.nav.profile')}</a>
                         </li>
                     )}
                     {loggedIn && (
-                        <button className="md:p-4 py-3 px-0 block" onClick={handleLogout}>Logout</button>
+                        <button className="md:p-4 py-3 px-0 block" onClick={handleLogout}>{t('header.nav.logout')}</button>
                     )}
                     {!loggedIn && (
                         <li>
-                            <a className="md:p-4 py-3 px-0 block" href="/login">Login</a>
+                            <a className="md:p-4 py-3 px-0 block" href="/login">{t('header.nav.login')}</a>
                         </li>
                     )}
                     {!loggedIn && (
                         <li>
-                            <a className="md:p-4 py-3 px-0 block" href="/register">Register</a>
+                            <a className="md:p-4 py-3 px-0 block" href="/register">{t('header.nav.register')}</a>
                         </li>
                     )}
                     <li>
