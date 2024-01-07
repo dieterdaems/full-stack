fcdc5da733e24795d4b668cf21ccde8d96140907
dieterdaems <113980664+dieterdaems@users.noreply.github.com>
2024-01-07 10:02:34 +0100

    added more front end tests

--- front-end/tests/header.test.tsx
+++ front-end/tests/header.test.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 
+
 window.React = React;
 
 jest.mock('next/router', () => ({
@@ -92,5 +93,105 @@ describe('Header', () => {
     // fireEvent.click(screen.getByTestId('login'));
     // expect(useRouter().push).toHaveBeenCalledWith('/login');
 });
+
+it('redirects to the register page when "Register" link is clicked', () => {
+  sessionStorage.clear();
+  render(<Header />);
+  const { t } = useTranslation();
+
+const registerLink = screen.getByText(t('header.nav.register'));
+const hrefValue = registerLink.getAttribute('href');
+expect(hrefValue).toEqual('/register');
+
+});
+
+it('redirects to the home page when "Home" link is clicked', () => {
+  sessionStorage.clear();
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.home')).getAttribute('href')).toEqual('/');
+
+});
+
+it('redirects to the users page when "Users" link is clicked as admin', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.users')).getAttribute('href')).toEqual('/users');
+
+});
+
+it('redirects to the teams page when "Teams" link is clicked as admin', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.teams')).getAttribute('href')).toEqual('/teams');
+
+});
+
+it('redirects to the teams page when "Teams" link is clicked as user', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '4b975fd8f0ff3e9fe958e701d5053be7dc223b684ec633f3d322d8868d395d33');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.teams')).getAttribute('href')).toEqual('/teams');
+
+});
+
+
+it('redirects to the projects page when "Projects" link is clicked as admin', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.projects')).getAttribute('href')).toEqual('/projects');
+
+});
+
+it('redirects to the projects page when "Projects" link is clicked as user', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '4b975fd8f0ff3e9fe958e701d5053be7dc223b684ec633f3d322d8868d395d33');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.projects')).getAttribute('href')).toEqual('/projects');
+
+});
+
+it('redirects to the profile page when "Profile" link is clicked as admin', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.profile')).getAttribute('href')).toEqual('/users/profile/');
+
+});
+
+it('redirects to the profile page when "Profile" link is clicked as user', () => {
+  sessionStorage.clear();
+  sessionStorage.setItem('loggedUser', 'admin');
+  sessionStorage.setItem('role', '4b975fd8f0ff3e9fe958e701d5053be7dc223b684ec633f3d322d8868d395d33');
+  render(<Header />);
+  const { t } = useTranslation();
+
+  expect(screen.getByText(t('header.nav.profile')).getAttribute('href')).toEqual('/users/profile/');
+
+});
+
+
 
 });
