b27ad9ffa448cf2484f3532b037d775fd2ec9a67
Ibra Altaee <altaee.ibra@gmail.com>
2024-01-07 06:06:06 +0100

    fetch all teams in dropdown if user is admin

--- front-end/pages/projects/index.tsx
+++ front-end/pages/projects/index.tsx
@@ -11,6 +11,7 @@ import Header from "@/components/header";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import UserService from "@/services/UserService";
+import TeamService from "@/services/TeamService";
 
 const Projects: React.FC = () => {
 
@@ -39,6 +40,19 @@ const Projects: React.FC = () => {
 
     const getUserTeams = async () => {
         if (!mayFetchTeams) return; // to prevent the unnecessary first call to getUserTeams() when the page is loaded
+        const role = sessionStorage.getItem("role");
+        // if the user is an admin, we need to fetch all teams. Otherwise, we only need to fetch the teams of the current user
+        if (role === '91fb3f8394dead2470aaf953e1bed9d9abf34a41f65ac666cff414ca229245b8') {
+            const response = await TeamService.getAll();
+            if (!response.ok) { // I didn't check for 401 here because as long as you're logged in, you can see all teams.
+                setErrorMessage(response.statusText);
+                return;
+            }
+            else {
+                const teams = await response.json();
+                return teams;
+            }
+        }
         const id = sessionStorage.getItem("loggedUser");
         const response = await UserService.getById(id);
         if (!response.ok) {
